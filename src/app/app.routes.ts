b9303dccc2ad3faeed62b0984475edd9156471bf
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-17 00:18:36 -0300

    add routes dashboard and list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES) },
+  { path: 'list', loadChildren: () => import('./pages/list/list.routes').then(m => m.LIST_ROUTES) }
+];

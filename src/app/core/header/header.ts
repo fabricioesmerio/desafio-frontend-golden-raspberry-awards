74a319d514ba5af33bc13bb6f694036c1db5e211
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-17 00:04:42 -0300

    add header in core directory

---
+++ src/app/core/header/header.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NzLayoutModule
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class Header {
+
+}

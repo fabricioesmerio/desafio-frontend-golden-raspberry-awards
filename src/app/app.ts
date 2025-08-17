f4fccc287003784b7b3dfc3849f823b0b5e6f1c2
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-17 00:20:21 -0300

    adjust app template structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { Header } from './core/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('golden-raspberry-awards');
+  isCollapsed = false;
 }

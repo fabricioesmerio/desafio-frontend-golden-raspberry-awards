45f912ee1fdfbc9962bdcab6faa1dc043db092ab
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-18 01:28:14 -0300

    add environment

--- src/app/pages/dashboard/producers-with-longest-shortest-interval/data-access/producers-with-longest-shortest-interval.service.ts
+++ src/app/pages/dashboard/producers-with-longest-shortest-interval/data-access/producers-with-longest-shortest-interval.service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { MaxMinWinIntervalForProducersResponse } from "../models/max-min-win-interval-producers-response";
-
+import { environment } from "../../../../../environments/environment";
 
 
 @Injectable({ providedIn: 'root' })
 export class ProducersWithLongestShortestIntervalService {
     private readonly httpClient = inject(HttpClient);
+    private readonly apiUrl = environment.apiUrl;
 
     get(): Observable<MaxMinWinIntervalForProducersResponse> {
-        return this.httpClient.get<MaxMinWinIntervalForProducersResponse>(`https://challenge.outsera.tech/api/movies/maxMinWinIntervalForProducers`);
+        return this.httpClient.get<MaxMinWinIntervalForProducersResponse>(`${this.apiUrl}/maxMinWinIntervalForProducers`);
     }
 }
--- src/app/pages/dashboard/top-studios-with-winners/data-access/top-studios-with-winners.service.ts
+++ src/app/pages/dashboard/top-studios-with-winners/data-access/top-studios-with-winners.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
 import { TopStudiosWithWinnersResponse } from "../models/top-studios-with-winners-response";
+import { environment } from "../../../../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class TopStudiosWithWinnersService {
 
     private readonly httpClient = inject(HttpClient);
+    private readonly apiUrl = environment.apiUrl;
 
     get(): Observable<TopStudiosWithWinnersResponse> {
-        return this.httpClient.get<{ studios: TopStudiosWithWinnersResponse }>('https://challenge.outsera.tech/api/movies/studiosWithWinCount')
+        return this.httpClient.get<{ studios: TopStudiosWithWinnersResponse }>(`${this.apiUrl}/studiosWithWinCount`)
             .pipe(
                 map(data => data.studios)
             );

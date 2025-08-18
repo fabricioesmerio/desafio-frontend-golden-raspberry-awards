3d57925e2e5e64226dbd8c8053d1740a82d115e7
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-18 11:12:40 -0300

    fix(test): update imports to fix failing unit test

--- src/app/pages/dashboard/dashboard.spec.ts
+++ src/app/pages/dashboard/dashboard.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Dashboard } from './dashboard';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { ListMovieWinnersByYear } from './list-movie-winners-by-year/list-movie-winners-by-year';
+import { ProducersWithLongestShortestInterval } from './producers-with-longest-shortest-interval/producers-with-longest-shortest-interval';
+import { TopStudiosWithWinners } from './top-studios-with-winners/top-studios-with-winners';
+import { YearsWithMultipleWinnersTable } from './years-with-multiple-winners-table/years-with-multiple-winners-table';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('Dashboard', () => {
+describe(Dashboard.name, () => {
   let component: Dashboard;
   let fixture: ComponentFixture<Dashboard>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Dashboard]
+      imports: [
+        Dashboard,
+        NzGridModule,
+        NzDividerModule,
+        YearsWithMultipleWinnersTable,
+        TopStudiosWithWinners,
+        ProducersWithLongestShortestInterval,
+        ListMovieWinnersByYear,
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;

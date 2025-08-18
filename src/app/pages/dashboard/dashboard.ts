f8c28afe4b7e43a93b5859318ed11e6e0da6faf4
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-18 01:30:55 -0300

    add new components

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { YearsWithMultipleWinnersTable } from './years-with-multiple-winners-table/years-with-multiple-winners-table';
+import { TopStudiosWithWinners } from './top-studios-with-winners/top-studios-with-winners';
+import { ProducersWithLongestShortestInterval } from './producers-with-longest-shortest-interval/producers-with-longest-shortest-interval';
+import { ListMovieWinnersByYear } from './list-movie-winners-by-year/list-movie-winners-by-year';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    NzGridModule,
+    NzDividerModule,
+    YearsWithMultipleWinnersTable,
+    TopStudiosWithWinners,
+    ProducersWithLongestShortestInterval,
+    ListMovieWinnersByYear
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })

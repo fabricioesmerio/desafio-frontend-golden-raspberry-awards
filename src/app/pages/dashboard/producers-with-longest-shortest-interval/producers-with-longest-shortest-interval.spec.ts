94e76404bab4bd7b11bb5b594ffbc1a854ac954a
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-08-18 10:12:57 -0300

    test: add unit test for producers with logest and shortest interval

--- src/app/pages/dashboard/producers-with-longest-shortest-interval/producers-with-longest-shortest-interval.spec.ts
+++ src/app/pages/dashboard/producers-with-longest-shortest-interval/producers-with-longest-shortest-interval.spec.ts
@@ -1,23 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProducersWithLongestShortestInterval } from './producers-with-longest-shortest-interval';
+import { ProducersWithLongestShortestIntervalService } from './data-access/producers-with-longest-shortest-interval.service';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { MaxMinWinIntervalForProducersResponse } from './models/max-min-win-interval-producers-response';
+import { of } from 'rxjs';
 
-describe('ProducersWithLongestShortestInterval', () => {
+fdescribe(ProducersWithLongestShortestInterval.name, () => {
   let component: ProducersWithLongestShortestInterval;
   let fixture: ComponentFixture<ProducersWithLongestShortestInterval>;
+  let service: jasmine.SpyObj<ProducersWithLongestShortestIntervalService>;
+
+  const mockResponse: MaxMinWinIntervalForProducersResponse = {
+    max: [
+      {
+        followingWin: 2010, interval: 10, previousWin: 2000, producer: 'Producer 1'
+      }
+    ],
+    min: [
+      {
+        followingWin: 1996, interval: 1, previousWin: 1995, producer: 'Producer 2'
+      }
+    ]
+  };
 
   beforeEach(async () => {
+
+    service = jasmine.createSpyObj('ProducersWithLongestShortestIntervalService', ['get']);
+    service.get.and.returnValue(of(mockResponse));
+
+
     await TestBed.configureTestingModule({
-      imports: [ProducersWithLongestShortestInterval]
+      imports: [ProducersWithLongestShortestInterval, NzTableModule],
+      providers: [
+        { provide: ProducersWithLongestShortestIntervalService, useValue: service }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProducersWithLongestShortestInterval);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`${ProducersWithLongestShortestInterval.prototype.setData.name}: should be invoked once during component initialization`, () => {
+    spyOn(component, 'setData');
+    fixture.detectChanges();
+
+    expect(component.setData).toHaveBeenCalled();
+    expect(component.setData).toHaveBeenCalledTimes(1);
+  })
+
+  it('should correctly populate the arrays listOfMax and listOfMin', () => {
+    fixture.detectChanges();
+
+    const minInterval = component.listOfMin.map(item => item.interval)[0];
+    const maxInterval = component.listOfMax.map(item => item.interval)[0];
+    const minIntervalMock = mockResponse.min.map(item => item.interval)[0];
+    const maxIntervalMock = mockResponse.max.map(item => item.interval)[0];
+
+    expect(component.loading).toBeFalse();
+    expect(component.listOfMin).toBeTruthy();
+    expect(component.listOfMax).toBeTruthy();
+    expect(minInterval).toBe(minIntervalMock);
+    expect(maxInterval).toBe(maxIntervalMock);
+  })
 });
